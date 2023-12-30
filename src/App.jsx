1e18362cc679bb72b1236834fd66f9ad02ea1f4d
Pedro Henrique Morais Pereira <pedromorais3012@gmail.com>
2023-12-30 18:11:12 -0300

    Understanding useRef and using 'marked' plugin to transform Markdown into HTML text

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import { useState, useEffect, useRef } from 'react'
 import ToolBar from './components/ToolBar/ToolBar'
+import { marked } from 'marked'
 
 function App() {
+  const [text, setText] = useState("# AA")
+  const textAreaRef = useRef(null)
+
+  const renderText = () => {
+    return {__html: marked(text)}
+  }
 
   return (
     <div className="app-container">
       <ToolBar />
-      <textarea></textarea>
+      <textarea 
+        ref={textAreaRef} 
+        value={text} 
+        onChange={(e) => setText(e.target.value)}
+      ></textarea>
 
-      <div className="markdown-content"></div>
-    </div>
+      <div className="markdown-content" dangerouslySetInnerHTML={renderText()} />
+    </div> 
   )
 }
 
--- src/components/ToolBar/ToolBar.jsx
+++ src/components/ToolBar/ToolBar.jsx
@@ -2,8 +2,15 @@ import React from "react"
 
 const ToolBar = () => {
    return(
-      <div>
-         ToolBar
+      <div className="toolbar">
+         <button>h1</button>
+         <button>h2</button>
+         <button>Bold</button>
+         <button>Italic</button>
+         <button>Link</button>
+         <button>Code Block</button>
+         <button>List Item</button>
+         <button>Horizontal Line</button>
       </div>
    )
 }
